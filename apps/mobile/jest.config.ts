006742e06b291a4605c6ac6997fb6373c429edbd
Karthikan <karthi.murugathas@flagstoneim.com>
2025-02-05 14:18:39 +0000

    fix jest issues

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -19,3 +19,4 @@ module.exports = {
   },
   coverageDirectory: '../../coverage/apps/mobile',
 };
+  
--- apps/mobile/src/test-setup.ts
+++ apps/mobile/src/test-setup.ts
@@ -1,1 +1,7 @@
+
+/* eslint-disable import/first */
+
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+
 import '@testing-library/jest-native/extend-expect';
+import './preloads/unistyles';
